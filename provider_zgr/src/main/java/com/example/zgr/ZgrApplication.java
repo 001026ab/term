58401ae1489825a39c9abbc43655625d877a69f7
张贵荣 <1176754512@qq.com>
2020-06-17 12:14:56 +0800

    Initial commit

---
+++ provider_zgr/src/main/java/com/example/zgr/ZgrApplication.java
@@ -0,0 +1,13 @@
+package com.example.zgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZgrApplication.class, args);
+    }
+
+}
