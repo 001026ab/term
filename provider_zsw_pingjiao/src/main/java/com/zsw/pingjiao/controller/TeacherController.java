6adc18ad1fd85885a478a6100048cc3f3d6214fd
ZHANGDOUBLEWEI <1390429173@qq.com>
2020-06-23 17:34:26 +0800

    provider_zsw_pingjiao--增加网关

--- provider_zsw_pingjiao/src/main/java/com/zsw/pingjiao/controller/TeacherController.java
+++ provider_zsw_pingjiao/src/main/java/com/zsw/pingjiao/controller/TeacherController.java
@@ -21,30 +21,30 @@ public class TeacherController {
     @Resource
     TeacherService teacherService;
 
-    @RequestMapping("/")
+    @RequestMapping("/zsw")
     public String index() {
 
-        return "redirect:/main";
+        return "redirect:/zsw/main";
     }
 
-    @RequestMapping("/main")
+    @RequestMapping("/zsw/main")
     public String list(Model model) {
         List<Teacher> teacheres=teacherService.getTeacherList();
         model.addAttribute("teacheres", teacheres);
         return "teacher/main";
     }
 
-    @RequestMapping("/topj")
+    @RequestMapping("/zsw/topj")
     public String pj(Model model,Integer id) {
         Teacher teacher=teacherService.findUserById(id);
         model.addAttribute("teacher", teacher);
         return "teacher/pingjiaoshi";
     }
 
-    @RequestMapping("/add")
+    @RequestMapping("/zsw/add")
     public String add(TeacherFenShu teacherFenShu,Integer id) {
         teacherService.save(teacherFenShu);
         teacherService.update(id);
-        return "redirect:/main";
+        return "redirect:/zsw/main";
     }
 }
